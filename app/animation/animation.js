c438ef810857d1e313fbdbebd39ceec9687d1d1a
6Botter7 <7williamsandre7@gmail.com>
2022-09-17 15:19:02 +0200

    Update to animation bar
    Fixed Failed to load source issue.
    
    Made component more dynamic with regards to the number of children

--- app/animation/animation.js
+++ app/animation/animation.js
@@ -13,9 +13,12 @@ export default class AnimateBar extends crsbinding.classes.ViewBase {
         console.log(this.container);
         await this.update();
         this.moveRightHandler = this.moveRight.bind(this);
-        this.interval = setInterval(this.moveHandler, 1000);
+        this.interval = setInterval(this.moveRightHandler, 3000);
         // this.container = this.element.querySelector(".container");
         // console.log(this.container);
+
+        this.allChildren = this.container.getElementsByTagName('div').length;
+        console.log(this.allChildren);
         
 
     }   
@@ -30,16 +33,19 @@ export default class AnimateBar extends crsbinding.classes.ViewBase {
     async update() {
         const startX = -600;
 
-        for(let i = 0; i < 4; i++) {
+        for(let i = 0; i < this.allChildren ; i++) {
             const element = this.container.children[i];
+            console.log(this.container.children[i])
             element.dataset.x = startX + 400;
             element.style.translate = `${startX + i *400}px`;
         }
     }
 
     async moveRight() {
+
+
         let set_timeout = setTimeout(() => {
-            for(let i = 0; i < 4; i++) {
+            for(let i = 0; i < this.allChildren; i++) {
                 const element = this.container.children[i];
                 const x = Number(element.dataset.x);
                 element.dataset.x = x + 400;
@@ -52,6 +58,8 @@ export default class AnimateBar extends crsbinding.classes.ViewBase {
             clearTimeout(clear_timeout);
             const element = this.container.removeChild(this.container.lastElementChild);
             this.container.insertBefore(element , this.container.firstElementChild);
+            
+            
 
             element.dataset.x = -600;
             element.style.translate = "-600px";
